f00c36212bec53094f3f31dc82dbad21338ea11d
Lisa Feng <lisafeng10@gmail.com>
2020-06-18 13:06:21 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -7,7 +7,7 @@ def summon_captain_planet(elements)
 end
 
 def long_planeteer_calls(calls)
-  calls.each.any?{|words| }
+  calls.each.any?{|word| word.length > 4}
 end
 
 def find_the_cheese# code an argument here
